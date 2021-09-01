61fa83b2dcf5a50cf744d48f481b00965bb93a24
nivereno@gmail.com <nivereno@gmail.com>
2021-09-02 01:54:48 +0300

    Fixed usings

--- TournamentTrackerLibrary/Models/MatchupEntryModel.cs
+++ TournamentTrackerLibrary/Models/MatchupEntryModel.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TournamentTrackerLibrary;
+using TournamentTrackerLibrary.Models;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.Models
 {
     public class MatchupEntryModel
     {
--- TournamentTrackerLibrary/Models/TeamModel.cs
+++ TournamentTrackerLibrary/Models/TeamModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TournamentTrackerLibrary
+namespace TournamentTrackerLibrary.Models
 {
     public class TeamModel
     {
