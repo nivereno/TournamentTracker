4ff901be3e454b6a0857aa1511b8a27b08c1ce82
nivereno@gmail.com <nivereno@gmail.com>
2021-09-17 17:13:24 +0300

    load rounds and populate lists

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TrackerLibrary.Models;
 
 namespace TrackerUI
 {
     public partial class TournamentViewerForm : Form
     {
-        public TournamentViewerForm()
+        private TournamentModel tournament;
+        List<int> rounds = new List<int>();
+
+        public TournamentViewerForm(TournamentModel tournamentModel)
         {
             InitializeComponent();
+
+            tournament = tournamentModel;
+
+            LoadFormData();
+
+            LoadRounds();
+        }
+
+        private void LoadFormData()
+        {
+            tournamentName.Text = tournament.TournamentName;
+        }
+
+        private void WireUpLists()
+        {
+            roundDropDown.DataSource = null;
+            roundDropDown.DataSource = rounds;
+        }
+        private void LoadRounds()
+        {
+            rounds = new List<int>();
+
+            //rounds.Add(1);
+            int currRound = 0;
+            foreach (List<MatchupModel> matchups in tournament.Rounds)
+            {
+                //if (matchups.First().MatchupRound > currRound)
+                //{
+                //    currRound = matchups.First().MatchupRound;
+                //    rounds.Add(currRound);
+                //}
+                currRound += 1;
+                rounds.Add(currRound);
+            }
+
+            WireUpLists();
         }
 
         private void label1_Click(object sender, EventArgs e)
